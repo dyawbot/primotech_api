c0e4bb9c1de78c41c63a22684dd4d0a7cb1485f9
dyawbot <jawarapaglinawan@gmail.com>
2025-12-19 16:19:47 +0800

    [webudget ] add new column update for all models

--- app/model/we_budget/base/timestamp_mixin.py
+++ app/model/we_budget/base/timestamp_mixin.py
@@ -2,5 +2,5 @@ from sqlalchemy import Column, DateTime, func
 
 class TimestampMixIn:
     created_at = Column(DateTime(timezone=True), server_default=func.now(),nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now(),onupdate=func.now(), nullable=True)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(),onupdate=func.now(), nullable=True)
 
